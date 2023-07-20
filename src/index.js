fb3b583d7657a3502ba88e47df8d87083bc7343c
Jesus Ramirez <jesusdevelp@gmail.com>
2023-07-19 20:39:39 -0600

    Backend init config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import dotenv from 'dotenv'
+import app from './app.js'
+
+// dot config
+dotenv.config()
+
+// env variables
+const PORT = process.env.PORT || 8000
+
+app.listen( PORT, () => {
+    console.log( `Server listening at ${ PORT }` )
+})
