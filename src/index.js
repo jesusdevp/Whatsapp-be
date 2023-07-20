3427261a0a032ed901acebdb93f55d289fb7c7cf
Jesus Ramirez <jesusdevelp@gmail.com>
2023-07-19 21:56:50 -0600

    Custom logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 import app from './app.js'
+import logger from './configs/logger.config.js'
 
 // env variables
 const PORT = process.env.PORT || 8000
 console.log(process.env.NODE_ENV)
 
 app.listen( PORT, () => {
-    console.log( `Server listening at ${ PORT }` )
+    logger.info( `Server listening at ${ PORT }` )
 })
